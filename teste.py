fdeb2494783d1cf523d004cf11f09f0b3ad40e8e
Luizcostadev <luiz.costa17@outlook.com>
2024-11-19 20:26:12 -0300

    peloamor de deus

---
+++ teste.py
@@ -0,0 +1,5 @@
+nota_do_professor= int(input('dê uma nota para wuldson: '))
+if nota_do_professor >= 7:
+    print('Wuldson é foda')
+elif nota_do_professor < 7:
+    print('Tu tem mal gosto pra professor')
